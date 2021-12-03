450ee73b1aa151f3fb55776dab11554f795c9892
Jayson Byrd <jayson_byrd_95@outlook.com>
2021-12-03 09:16:55 -0500

    Final Test Completed - Sprint Completed

--- src/components/CheckoutForm.test.js
+++ src/components/CheckoutForm.test.js
@@ -1,7 +1,8 @@
 import React from "react";
 import MutationObserver from 'mutationobserver-shim';
-import { render, screen } from "@testing-library/react";
+import { render, screen} from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
+import userEvent from '@testing-library/user-event'
 
 // Write up the two tests here and make sure they are testing what the title shows
 
@@ -24,4 +25,25 @@ test("renders without errors", () => {});
   expect(zip).toBeInTheDocument();
   expect(submit).toBeInTheDocument();
 
-test("shows success message on submit with form details", () => {});
+test("shows success message on submit with form details", () => {
+  render(<CheckoutForm/>);
+  const fName = screen.queryByLabelText('First Name:');
+  const lName = screen.queryByLabelText('Last Name:');
+  const address = screen.queryByLabelText('Address:');
+  const city = screen.queryByLabelText('City:');
+  const state = screen.queryByLabelText('State:');
+  const zip = screen.queryByLabelText('Zip:');
+  const submit = screen.queryByText(/^checkout$/i);
+
+  userEvent.type(fName,'Jayson');
+  userEvent.type(lName,'Byrd');
+  userEvent.type(address,'302 Some Street');
+  userEvent.type(city,'Anna');
+  userEvent.type(state,'Ohio');
+  userEvent.type(zip,'45302');
+  userEvent.click(submit);
+
+  const succ = screen.getByTestId('successMessage');
+
+  expect(succ).toBeInTheDocument();
+});
